34306f52103258df34c6ff63f175affc9b489e1c
Anna Griffith <anna.gr@live.co.uk>
2023-10-21 19:04:24 +0100

    pybind setup

---
+++ build/CMakeFiles/cmake_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_example.dir/pywrap.cpp.o"
+  "cmake_example.cpython-36m-darwin.so"
+  "cmake_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/temp.macosx-10.9-x86_64-3.6/cmake_example/CMakeFiles/cmake_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib.macosx-10.9-x86_64-3.6/cmake_example.cpython-36m-darwin.so"
+  "../../lib.macosx-10.9-x86_64-3.6/cmake_example.pdb"
+  "CMakeFiles/cmake_example.dir/pywrap.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
